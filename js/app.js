597254b33fa088723a15ea54ecc4dced566e863a
MohammadAljadayh <mjedayh@gmail.com>
2021-07-06 16:32:23 +0300

    add new 2 qustion to the code

--- js/app.js
+++ js/app.js
@@ -1,73 +1,119 @@
 'use strict'
 
-let userName=prompt('what\'s your name?');
+let userName = prompt('what\'s your name?');
 
-console.log('this is for user name',userName);
+console.log('this is for user name', userName);
 
-alert('hello '+userName); 
+alert('hello ' + userName);
+let score = 0;
 
-let age =prompt('Are You Age  Between 20 to 30 ');
-age=age.toLowerCase();
+let age = prompt('Are You Age  Between 20 to 30 ');
+age = age.toLowerCase();
 
-if(age=='yes' || age=='y')
-{
-alert(' Great You Chice yes  your agg between 20-30 Name Is') ;
+if (age == 'yes' || age == 'y') {
+    alert(' Great You Chice yes  your agg between 20-30 Name Is');
+    score++;
 }
-else
-{
-    alert(' You Chice No ') ;
-}
-
-let animal=prompt('are you like cat');
-
-animal=animal.toLowerCase();
-
-if(animal=='yes' || animal=='y')
-{
-alert(' Great You Like Cat') ;
-}
-else
-{
-    alert(' Me Too :) ') ;
+else {
+    alert(' You Chice No ');
 }
 
-let car=prompt('are you like mazda car');
+let animal = prompt('are you like cat');
 
-car=car.toLowerCase();
+animal = animal.toLowerCase();
 
-if(car=='yes' || car=='y')
-{
-alert(' Great You Like Mazda Car') ;
+if (animal == 'yes' || animal == 'y') {
+    alert(' Great You Like Cat');
+    score++;
 }
-else
-{
-    alert('Also  Me Too :) ') ;
-}
-
-let mobile=prompt('are you like iphone Phone') 
-
-mobile=mobile.toLowerCase();
-
-if(mobile=='yes'|| mobile=='y')
-{
-alert(' Great You Like Mazda Car') ;
-}
-else
-{
-    alert('Also  Me Too :) ') ;
+else {
+    alert(' Me Too :) ');
 }
 
-let LTUC=prompt('are you like LTUC') 
+let car = prompt('are you like mazda car');
 
-LTUC=LTUC.toLowerCase();
+car = car.toLowerCase();
 
-if(LTUC=='yes'|| LTUC=='y')
-{
-alert(' Great ^^  ') ;
+if (car == 'yes' || car == 'y') {
+    alert(' Great You Like Mazda Car');
+    score++;
 }
-else
-{
-    alert('oh no this one of the best college in the jordan') ;
+else {
+    alert('Also  Me Too :) ');
 }
 
-alert(' Great' +" " + userName + " "+ 'thanks for your answers' );
+let mobile = prompt('are you like iphone Phone')
+
+mobile = mobile.toLowerCase();
+
+if (mobile == 'yes' || mobile == 'y') {
+    alert(' Great You Like Mazda Car');
+    score++;
+}
+else {
+    alert('Also  Me Too :) ');
+}
+
+let LTUC = prompt('are you like LTUC')
+
+LTUC = LTUC.toLowerCase();
+
+if (LTUC == 'yes' || LTUC == 'y') {
+    alert(' Great ^^  ');
+    score++;
+}
+else {
+    alert('oh no this one of the best college in the jordan');
+}
+
+alert(' you have 4 chance to guess number. between 1-10 ');
+
+for (let i = 0; i < 4; i++) {
+    console.log("we are here ")
+
+    let Number = prompt('try to guess number. between 1-10 to check if YOU are Lucky or not :)')
+    if (Number != 7 && i == 3) {
+        alert(' oh no .. no chance you have now the correct answers is 7');
+    }
+    else if (Number > 7) {
+        alert(' the guess number is too high ');
+    }
+
+    else if (Number < 7) {
+        alert(' the guess number is too low ');
+    }
+
+    else if (Number == 7) {
+        alert('  Great You Are Vary Lucky');
+        score++;
+        break;
+    }
+
+}
+
+alert(' you have 6 chance to guess one of the best Fruits');
+
+let Fruits = ['banana', 'apple', 'grape', 'orange', 'Watermelon'];
+
+var loopc=false;
+for (let i = 0; i < 6 && !loopc;i++) {
+    let Fruite = prompt('try to guess  some of the  best Fruits :)')
+    Fruite = Fruite.toLowerCase();
+
+    for (let f = 0; f < Fruits.length; f++) {
+        if (Fruite == Fruits[f]) {
+
+            alert('Great You Are Vary Lucky');
+            score++;
+            loopc=true;
+            break;
+        }
+
+    }
+
+}
+
+alert("all the true answers for the 7th qustion one of these fruits 'banana'+ 'apple'+ 'grape'+ 'orange'+'Watermelon' " )
+
+alert("your score is " + " " + score);
+
